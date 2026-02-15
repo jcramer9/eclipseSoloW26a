e454ada6aebf4d5281d341ba5382b2b174ce2d6e
jcramer9 <jcramer9@emich.edu>
2026-02-15 15:16:22 -0500

    Programmer 1 changed some things!

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		
 		System.out.println("This will be the second commit!");
 		
-		System.out.println("Programmer 2 did this!!!");
+		System.out.println("Programmer 2 did this!!! programmer 1 did this!");
 	}
 
 }
